6d81848b177db33cc426db6a40edbc53b954152c
github username <github email address>
2019-09-24 13:06:49 -0400

    Add formContainer state and handleFormChange

--- src/components/DeliveryForm.js
+++ src/components/DeliveryForm.js
@@ -7,38 +7,38 @@ class DeliveryForm extends Component {
   render() {
     return(
       <div>
-        <Form>
+        <Form onChange={this.props.handleFormChange}>
           <FormGroup>
-            <Label for="exampleAddress">Address</Label>
-            <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+            <Label for="address1">Address</Label>
+            <Input type="text" name="address1" id="address1" value={this.props.address1} />
           </FormGroup>
           <FormGroup>
-            <Label for="exampleAddress2">Address 2</Label>
-            <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+            <Label for="address2">Address 2</Label>
+            <Input type="text" name="address2" id="address2" placeholder="Apartment, studio, or floor" value={this.props.address2}/>
           </FormGroup>
           <Row form>
             <Col md={6}>
               <FormGroup>
-                <Label for="exampleCity">City</Label>
-                <Input type="text" name="city" id="exampleCity"/>
+                <Label for="city">City</Label>
+                <Input type="text" name="city" id="city" value={this.props.city}/>
               </FormGroup>
             </Col>
             <Col md={4}>
               <FormGroup>
-                <Label for="exampleState">State</Label>
-                <Input type="text" name="state" id="exampleState"/>
+                <Label for="state">State</Label>
+                <Input type="text" name="state" id="state" value={this.props.state}/>
               </FormGroup>
             </Col>
             <Col md={2}>
               <FormGroup>
-                <Label for="exampleZip">Zip</Label>
-                <Input type="text" name="zip" id="exampleZip"/>
+                <Label for="zip">Zip</Label>
+                <Input type="text" name="zip" id="zip" value={this.props.zip}/>
               </FormGroup>
             </Col>
           </Row>
           <FormGroup>
             <Label for="restaurant">Restaurant</Label>
-            <Input type="text" name="restaurant" id="restaurant"/>
+            <Input type="text" name="restaurant" id="restaurant" value={this.props.restaurant}/>
           </FormGroup>
           <Button>Search</Button>
         </Form>
--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -3,11 +3,33 @@ import DeliveryForm from './DeliveryForm'
 
 class FormContainer extends Component {
 
+  state={
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
+    restaurant: ""
+  }
+
+  handleFormChange=event=>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
     return(
       <div>
-        <DeliveryForm />
+        <DeliveryForm
+          address1={this.state.address1}
+          address2={this.state.address2}
+          city={this.state.city}
+          state={this.state.state}
+          zip={this.state.zip}
+          restaurant={this.state.restaurant}
+          handleFormChange={this.handleFormChange}
+          />
       </div>
     )
   }
